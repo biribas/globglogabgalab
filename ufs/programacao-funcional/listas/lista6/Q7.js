505cada6ee798118a0a413e45e9d665c7a37cdbb
saintwalker2814 <cadrianogamac@gmail.com>
2022-04-28 12:19:21 -0300

    Improve readability

--- ufs/programacao-funcional/listas/lista6/Q7.js
+++ ufs/programacao-funcional/listas/lista6/Q7.js
@@ -5,8 +5,8 @@
   f(m, n) = f(m - n, n), se m > n
 */
 
-const f = (m, n) => m < 0 || n <= 0 || !Number.isInteger(n) || !Number.isInteger(m) ? "Valor inválido" : fAux(m, n);
+const resto = (m, n) => m < 0 || n <= 0 || !Number.isInteger(n) || !Number.isInteger(m) ? "Valor inválido" : fAux(m, n);
 
-const fAux = (m, n) => m < n ? m : fAux(m - n, n);
+const restoAux = (m, n) => m < n ? m : fAux(m - n, n);
 
 console.log(f(5, 2));
