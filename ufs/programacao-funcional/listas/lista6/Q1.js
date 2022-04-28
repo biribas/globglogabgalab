995c0d80df1bf9624e7943532bbb27c0256b41cf
saintwalker2814 <cadrianogamac@gmail.com>
2022-04-28 10:55:32 -0300

    Refactoring

--- ufs/programacao-funcional/listas/lista6/Q1.js
+++ ufs/programacao-funcional/listas/lista6/Q1.js
@@ -1,7 +1,13 @@
-// f(1) = 3
-// f(n) = 2 * f(n - 1)
+// Q1. N-ésimo termo da sequência {3,6,12,24,48,...}.
+/*
+ Formulação recursiva
+  f(1) = 3
+  f(n) = 2 * f(n - 1)
+*/
 
-const f = n => n == 1 ? 3 : 2 * f(n - 1);
+const f = n => n < 1 || !Number.isInteger(n) ? "Valor inválido" : fAux(n);
+
+const fAux = n => n == 1 ? 3 : 2 * fAux(n - 1);
 
 console.log(f(5));
 
