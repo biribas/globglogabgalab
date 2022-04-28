fce218719ea45b6f851973c5e21855a66d4292e8
saintwalker2814 <cadrianogamac@gmail.com>
2022-04-28 15:00:09 -0300

    Improve Readability

--- ufs/programacao-funcional/listas/lista6/Q3.js
+++ ufs/programacao-funcional/listas/lista6/Q3.js
@@ -7,10 +7,10 @@
   f([a1, a2, ...an]) = f([a2, ...an])
 */
 
-const f = ([a1, ...an]) => !a1 ? "Lista vazia" : fAux([a1, ...an]);
+const last = ([a1, ...an]) => !a1 ? "Lista vazia" : lastAux([a1, ...an]);
 
-const fAux = ([a1, ...an]) => an.length == 0 ? a1 : fAux(an);
+const lastAux = ([a1, ...an]) => an.length == 0 ? a1 : lastAux(an);
 
 const lista = [1, 4, 5, 2, 3];
 
-console.log(f(lista));
+console.log(last(lista));
