ed208e8265b33b7f06d4cf9116d7b3604fe78ce5
saintwalker2814 <cadrianogamac@gmail.com>
2022-04-28 12:24:47 -0300

    Some bugs fixed

--- ufs/programacao-funcional/listas/lista6/Q7.js
+++ ufs/programacao-funcional/listas/lista6/Q7.js
@@ -5,8 +5,10 @@
   f(m, n) = f(m - n, n), se m > n
 */
 
-const resto = (m, n) => m < 0 || n <= 0 || !Number.isInteger(n) || !Number.isInteger(m) ? "Valor inválido" : fAux(m, n);
+const resto = (m, n) => m < 0 || n <= 0 || !Number.isInteger(n) || !Number.isInteger(m) ? "Valor inválido" : restoAux(m, n);
 
-const restoAux = (m, n) => m < n ? m : fAux(m - n, n);
+const restoAux = (m, n) => m < n ? m : restoAux(m - n, n);
 
-console.log(f(5, 2));
+console.log(resto(5, 2));
+
+module.exports = {resto};
