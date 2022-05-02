64e63a6d1ae15b2aeeab314b4153621fe974050b
saintwalker2814 <cadrianogamac@gmail.com>
2022-05-02 14:54:03 -0300

    Refactoring

--- ufs/programacao-funcional/listas/lista7/myMap.js
+++ ufs/programacao-funcional/listas/lista7/myMap.js
@@ -1,20 +1,20 @@
 // Reimplementação do método Array.prototype.map()
 
 Array.prototype.myMap = function(callback, thisArg) {
-    if (this == null)
+    if (this === null)
         throw new TypeError("this is null or not defined");
     
     return map(callback, this, this, 0, thisArg);
 }
 
-const map = (callback, orginalArray, [head, ...tail], index, thisArg) => {
-    if (head == undefined)
+const map = (callback, originalArray, [head, ...tail], index, thisArg) => {
+    if (head === undefined)
         return [];
     
-    const mappedValue = callback.call(thisArg, head, index, orginalArray);
-    return [mappedValue, ...map(callback, orginalArray, tail, index + 1, thisArg)];
+    const mappedValue = callback.call(thisArg, head, index, originalArray);
+    return [mappedValue, ...map(callback, originalArray, tail, index + 1, thisArg)];
 }
 
 const mappedArray = [0, 1, 2, 3, 4, 5].myMap((e, i, array) => e * i);
 
-console.log(mappedArray);
+console.log(mappedArray); // Output: [0, 1, 4, 9, 16, 25]
