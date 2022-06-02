1b228f3f2af64c03d46bf47d5b5f15e1e4953b00
Carlos Adriano <cadrianogamac@gmail.com>
2022-06-02 17:08:33 -0300

    Create playRound function

--- odin-project/projects/rock-paper-scissors/script.js
+++ odin-project/projects/rock-paper-scissors/script.js
@@ -1,6 +1,31 @@
-const LIST = ['rock', 'paper', 'scissors'];
+const LIST = ['Rock', 'Paper', 'Scissors'];
 
-function computerPlay () {
+function computerPlay() {
   const index = Math.floor(Math.random() * 3);
   return LIST[index];
 }
+
+function playRound(playerSelection, computerSelection) {
+  const playerIndex = findIndex(playerSelection);
+  const computerIndex = findIndex(computerSelection);
+
+  const value = playerIndex - computerIndex;
+  
+  if (value === 0)
+    return 'It\'s a tie!';
+  
+  playerSelection = LIST[playerIndex];
+  
+  if (value === 2 || value === -1)
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  
+  return `You Win! ${playerSelection} beats ${computerSelection}`;
+}
+
+function findIndex(element) {
+  return LIST.findIndex(e => e.toLowerCase() === element.toLowerCase());
+}
+
+const playerSelection = 'rock';
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
