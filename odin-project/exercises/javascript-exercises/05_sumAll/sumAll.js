97ade03750b3e26a6ebdee9580c8b69948a88270
Carlos Adriano <cadrianogamac@gmail.com>
2022-06-03 15:35:24 -0300

    Question completed

--- odin-project/exercises/javascript-exercises/05_sumAll/sumAll.js
+++ odin-project/exercises/javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (typeof num1 != 'number' || typeof num2 != 'number' || num1 < 0 || num2 < 0) 
+    return 'ERROR';
 
+  if (num1 > num2)
+    return aux(num2, num1);
+
+  return aux(num1, num2);
+
+  function aux (n1, n2) {
+    if (n1 == n2) return n1;
+    return n1 + aux(n1 + 1, n2);
+  }
 };
 
 // Do not edit below this line
