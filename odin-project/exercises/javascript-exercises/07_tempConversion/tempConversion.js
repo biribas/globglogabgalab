e05b05c211ad3132e2dfe9582373a64db4df52b6
Carlos Adriano <cadrianogamac@gmail.com>
2022-06-03 15:54:56 -0300

    Question completed

--- odin-project/exercises/javascript-exercises/07_tempConversion/tempConversion.js
+++ odin-project/exercises/javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,10 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  const result = (temp - 32) * (5 / 9);
+  return result.toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  const result = ((9 / 5) * temp + 32, 1);
+  return result.toFixed(1);
 };
+
 
 // Do not edit below this line
 module.exports = {
