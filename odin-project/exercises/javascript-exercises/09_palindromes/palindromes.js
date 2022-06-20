44de0765511673216647e437ac88c7f2e83813bd
Carlos Adriano <cadrianogamac@gmail.com>
2022-06-20 12:16:58 -0300

    Question completed

--- odin-project/exercises/javascript-exercises/09_palindromes/palindromes.js
+++ odin-project/exercises/javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function(string) {
+  string = string.toLowerCase().replace(/[^a-z]/g, '');
+  const reverseString = [...string].reverse().join('');
+  return string === reverseString;
 };
 
 // Do not edit below this line
