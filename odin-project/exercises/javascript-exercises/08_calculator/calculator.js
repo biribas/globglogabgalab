08068ec293f9622c90a6a4e3c5ab682fbd5f42d0
Carlos Adriano <cadrianogamac@gmail.com>
2022-06-20 12:03:28 -0300

    Question completed

--- odin-project/exercises/javascript-exercises/08_calculator/calculator.js
+++ odin-project/exercises/javascript-exercises/08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, e) => total + e, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, e) => total * e, 1);
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	return num ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num == 0) return 1;
+	return num * factorial(num - 1);
 };
 
 // Do not edit below this line
